5293431b4f7baaa5d2c1b1271ff9100ab6c4cde8
MachuaninEzequiel <ezequieladrianmachuanin@gmail.com>
2023-04-30 08:56:45 -0300

    primer commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Clientes } from './clientes/Clientes';
+export { default as Proveedores } from './proveedores/Proveedores';
+export { default as Empleado } from './empleado/Empleado';
+export { default as Recuadro } from './recuadro/Recuadro';
