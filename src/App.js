9fdf84016925aa0d6e585d5b2d5b42d17f898bc6
Lautaro Robales <lautarorobales31@gmail.com>
2023-05-13 16:43:44 -0300

    pre bootstap

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-import {
-  Clientes,
-  Empleado,
-  Recuadro,
-  Search,
-  ListaClientes,
-  ListaProveedores,
-  Modale,
-  Login,
-  Login1,
-  Navbar,
-  Navbar1,
-  Navbar2,
-} from "./components";
-
-const App = () => {
+function App() {
   return (
-    <div>
-      <div className="gradient__bg">
-        <Navbar1 />
-      </div>
-      <div>
-        <Empleado />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
